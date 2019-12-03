e52b99e1a8d7c782e4c92b204351d72020f4d68b
jjerion <56169080+jjerion@users.noreply.github.com>
2019-12-03 14:23:55 -0500

    added script to change text

---
+++ CodeLab0Final/Assets/Scripts/TextInit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
+
+public class TextInit : MonoBehaviour
+{
+    public Text dialogue;
+
+    public string blank;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogue = GetComponent<Text>();
+        dialogue.text = " ";
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
