70dc6d076ed4a5657993f0ad6bdbf56bec007c0f
jjerion <56169080+jjerion@users.noreply.github.com>
2019-12-09 14:20:39 -0500

    added rough zoom

---
+++ CodeLab0Final/Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    
+    public void Zoom()
+    {
+        if (Camera.main.fieldOfView >= 53)
+        { 
+            InvokeRepeating("IncreaseZoom", 2, 5); //call IncreaseZoom every 5 seconds
+
+        }   
+    }
+ 
+    public void IncreaseZoom()
+    {
+        Camera.main.fieldOfView -= 1; //reduce fieldOfView by 1 (results in rough zoom (not smooth))
+    }
+}
